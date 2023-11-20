52efbdd37f85dc29febce4d27d657904886861ff
JoseArguinzones <145572262+JoseArguinzones@users.noreply.github.com>
2023-11-20 23:59:20 +0000

    random

--- src/app.js
+++ src/app.js
@@ -7,5 +7,12 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // console.log("Hello Rigo from the console!");
+  let palos = ["♦", "♥", "♠", "♣"];
+  let numeros = [2, 3, 4, 5, 6, 7, 8, 9, "A", "J", "Q", "K"];
+
+  let palorandom = palos[Math.floor(Math.random() * palos.length)];
+  let numerosrandom = numeros[Math.floor(Math.random() * numeros.length)];
+
+  console.log(palorandom, numerosrandom);
 };
