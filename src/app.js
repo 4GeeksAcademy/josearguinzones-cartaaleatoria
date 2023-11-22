b11653cc986959f40fd1003f9a7687d4732cdc5f
JoseArguinzones <145572262+JoseArguinzones@users.noreply.github.com>
2023-11-22 18:17:23 +0000

    aleatoria

--- src/app.js
+++ src/app.js
@@ -13,6 +13,19 @@ window.onload = function() {
 
   let palorandom = palos[Math.floor(Math.random() * palos.length)];
   let numerosrandom = numeros[Math.floor(Math.random() * numeros.length)];
+  let mostrarpalo = `${palorandom} `;
+  let mostrarnumero = `${numerosrandom}`;
+  let mostrarpalo2 = `${palorandom} `;
+  // const colors = ["red", "black"];
+  // let random = Math.floor(Math.random() * colors.length);
+  // palo.style.color = colors[random];
+  // palo2.style.color = colors[random];
 
-  console.log(palorandom, numerosrandom);
+  document.getElementById("palo").innerHTML = mostrarpalo;
+  document.getElementById("numero").innerHTML = mostrarnumero;
+  document.getElementById("palo2").innerHTML = mostrarpalo2;
+  if (palorandom == "♥" || palorandom == "♦") {
+    document.getElementById("palo").style.color = "red";
+    document.getElementById("palo2").style.color = "red";
+  }
 };
